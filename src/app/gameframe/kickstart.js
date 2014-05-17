f8ac06e197de2a4e1aa6c9ba719aa8ce2fc05786
Mikael Lepistö <mikael.lepisto@vincit.fi>
2014-05-17 23:48:28 +0300

    Kind of finished API + controls for starting new game.

--- src/app/gameframe/kickstart.js
+++ src/app/gameframe/kickstart.js
@@ -31,24 +31,61 @@ angular.module( 'gameframe.kickstart', [
 		profiles.add(name);
 	};
 	$scope.deleteProfile = function (name) {
+		// TODO: don't allow removing selectedProfile...
 		profiles.remove(name);
 	};
+
+	/**
+	 * Select profile related callbacks
+	 */ 
 	$scope.selectProfile = function (name) {
 		$scope.selectedProfile = profiles.get(name);
 	};
 	$scope.logOut = function () {
 		$scope.selectedProfile = null;
 	};
-
-	// if profile is selected show campaigns 
 	$scope.$watch('selectedProfile', function () {
 		$scope.campaigns = campaigns.list($scope.selectedProfile);
 	});
 
+	/**
+	 * Select campaign related callbacks
+	 */ 
 	$scope.selectCampaign = function (campaign) {
 		$scope.selectedCampaign = campaign;
 	};
+	$scope.$watch('selectedCampaign', function () {
+		$scope.levels = $scope.selectedCampaign.levels();
+	});
 
+	/**
+	 * Start / end level etc.
+	 */
+	$scope.selectLevel = function (level) {
+		$scope.selectedLevel = level;
+		$scope.aiCode = $scope.selectedProfile.getLevelCode(level);
+	};
+	$scope.startLevel = function () {
+		// start starts new game for canvas or if game is going on, 
+		// re-starts it and earlier game will be aborted
+		$scope.selectedGame.start($scope.aiCode, $scope.gameCanvas).win(function () {
+			console.log("You won!");
+		}).lose(function() {
+			console.log("You lose!");
+		}).abort(function () {
+			console.log("Aborted game.");
+		});
+	};
+	$scope.stopLevel = function () {
+		$scope.selectedGame.stop();
+	};
+
+	// restart game if code is changed
+	$scope.$watch('aiCode', function (newVal) {
+		if (_.isStr(newVal) && newVal.trim().length > 0) {
+			$scope.startGame();
+		}
+	});
 }])
 
 ;
